ea779e8de83f05754f8d78cb1cd68118005587c7
anhvu2501 <vu25ck28@gmail.com>
2021-06-06 00:10:09 +0700

    initial meanshift

--- seGroup2/src/se/project/clustering/meanshift/MeanShiftClustering.java
+++ seGroup2/src/se/project/clustering/meanshift/MeanShiftClustering.java
@@ -1,4 +1,44 @@
 package se.project.clustering.meanshift;
 
-public class MeanShiftClustering {
+import se.project.components.Cluster;
+import se.project.components.Point;
+
+import java.util.ArrayList;
+
+public class MeanShiftClustering extends Cluster {
+    private Point point;
+
+    public MeanShiftClustering(Point point) {
+        this.point = point;
+    }
+
+    @Override
+    public ArrayList<Point> finalResult() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Point> step() {
+        return null;
+    }
+
+    private double distance(Point source, Point des) {
+        double squareX = Math.pow(des.getX() - source.getY(), 2); //x^2
+        double squareY = Math.pow(des.getY() - source.getX(), 2); //y^2
+        return Math.sqrt(squareX + squareY);
+    }
+
+    private double kernel(double distance, int bandwidth) {
+        double squareDistance = Math.pow(distance, 2);
+        double squareBandwidth = Math.pow(bandwidth, 2);
+        return Math.pow(Math.E, -0.5 * (squareDistance / squareBandwidth));
+    }
+
+    public ArrayList<Point> meanShiftClustering(Point point, int bandwidth) {
+        return null;
+    }
+
+    public Point shift() {
+        return null;
+    }
 }
