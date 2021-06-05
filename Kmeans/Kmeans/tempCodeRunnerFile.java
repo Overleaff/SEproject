a9e475beb49d1a3d42832f2981e318fb1bc3f1ed
Overleaff <71872850+Overleaff@users.noreply.github.com>
2021-06-05 11:06:03 +0700

    delete

--- Kmeans/Kmeans/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-        series2.getData().add(new XYChart.Data(5.2, 229.2));
-        series2.getData().add(new XYChart.Data(2.4, 37.6));
-        series2.getData().add(new XYChart.Data(3.2, 49.8));
-
-        series1.getData().add(new XYChart.Data(9.9, 287.1));
-        series1.getData().add(new XYChart.Data(0.9, -9));
-        series1.getData().add(new XYChart.Data(3.2, 150.8));
-        series1.getData().add(new XYChart.Data(4.8, 20.8));
